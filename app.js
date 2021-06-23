e21c2bda6a040889758dddebec1d8d6bb1558645
Alice-Retz <alicegretz@gmail.com>
2021-06-23 17:53:01 -0500

    Cat functionality added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // import functions
 
 // reference needed DOM elements
+const playCat = document.getElementById('play-cat');
+const catPic = document.getElementById('cat-pic');
+const catSound = document.getElementById('cat-sound');
+catPic.addEventListener('click', ()=>{
+catSound.play()
+})
 
 
 // set event listeners 
