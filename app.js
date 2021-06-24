7fe9a25e2a88e8b85c2d8f015ac260ff4415f048
Alice-Retz <alicegretz@gmail.com>
2021-06-23 19:41:50 -0500

    Animation and Keystroeks added

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 // import functions
 
 // reference needed DOM elements
-const playCat = document.getElementById('play-cat');
-const catPic = document.getElementById('cat-pic');
+const catPic = document.getElementById('catpic');
 const catSound = document.getElementById('cat-sound');
 catPic.addEventListener('click', ()=>{
-catSound.play()
+    catSound.play();
+});
+const dogPic = document.getElementById('dog-pic');
+const dogSound = document.getElementById('dog-sound');
+dogPic.addEventListener('click', ()=>{
+    dogSound.play();
+});
+const horsePic = document.getElementById('horse-pic');
+const horseSound = document.getElementById('horse-sound');
+horsePic.addEventListener('click', ()=>{
+    horseSound.play();
+});
+dogPic.onclick = function() {
+catPic.classList.add('animate');
+};
+
+document.addEventListener('keydown', event => {
+  console.log(event.code);
+  if(event.code==="KeyA"){
+    catPic.classList.add('animate');
+    dogSound.play(); 
+  }
+
 })
+
+/*function logKey(e) {
+catPic.classList.add('animate');
+dogSound.play();
+};*/
+
 
 
 // set event listeners 
